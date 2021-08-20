0ca7adaab4d91da481ea60a55905172e57119351
sourovsobhan <sobhansourov737@gmail.com>
2021-08-21 03:36:58 +0600

    comment added

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
+// common function reset default value
 function costZero(convertZero){
     const commonElement = document.getElementById(convertZero);
     commonElement.innerText = 0;
     const totalMemory =document.getElementById('total');
     totalMemory.innerText = 1299;
 };
+
+// common function reset default value end
+// cost-money
 document.getElementById('convert-zero').addEventListener('click',function(){
     costZero('cost-memory');
 });
+
+
+// extra-cost-money
 document.getElementById('convert-storage-zero').addEventListener('click',function(){
 costZero('extra-cost-memory');
 });
+
+// delivery-crg
 document.getElementById('choose-convert-zero').addEventListener('click',function(){
    costZero('delivery-crg');
 });
+
+
+
+// common function of prices input
 
 function getCalculate(id,price){
     const element = document.getElementById(id);
@@ -22,14 +35,17 @@ function getCalculate(id,price){
         totalMemory.innerText = currentTotal;
 
 }
+
+//  memory-button 
 document.getElementById('second-memory-btn').addEventListener('click',function(){
    getCalculate('cost-memory',180);
 });
-
+//  storage-button
 document.getElementById('second-storage-btn').addEventListener('click',function(){
 getCalculate('extra-cost-memory',100);
 });
 
+//  -third-storage-button
 document.getElementById('Third-storage-btn').addEventListener('click',function(){
     getCalculate('extra-cost-memory',180);
 });
@@ -37,6 +53,9 @@ document.getElementById('Third-storage-btn').addEventListener('click',function()
 document.getElementById('storage-cost-btn').addEventListener('click',function(){
         getCalculate('delivery-crg',20);
 });
+
+// promo-code function
+
 
 document.getElementById('button-addon').addEventListener('click',function(){
 const inputField = document.getElementById('input-value');
